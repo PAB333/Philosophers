ffa0d63d5b046997427adc258c148912f46a5425
PAB333 <pabrd3@gmail.com>
2025-07-16 20:54:10 +0200

    start

---
+++ includes/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pibreiss <pibreiss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 20:48:38 by pibreiss          #+#    #+#             */
+/*   Updated: 2025/07/16 20:53:20 by pibreiss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
